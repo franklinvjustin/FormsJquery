ca6e801bad4ac2ff5a57fba578949692385988c3
franklinvjustin <35752919+franklinvjustin@users.noreply.github.com>
2022-01-31 18:17:27 +0530

    Add files via upload

--- form/JavaScript/form.js
+++ form/JavaScript/form.js
@@ -1,56 +1,221 @@
+var value = false;
+var submit = false;
+var count = false;
+
 function display() {
-    var x = document.getElementById("value").value;
-    if (x.length < 6) {
-        window.alert("Length of Name must be more than 6");
+    submit = true;
+    count = false;
+    checkPin();
+    checkName();
+    checkAddress();
+    checkEmail();
+    //checkMonth();
+    //checkDate();
+    checkPassword();
+    checkNumber();
+    checkFile();
+    checkCheck();
+    checkRadio();
+    submit = false;
+}
+
+function checkPassword() {
+    var pass = document.getElementById("pass").value;
+    if (pass == "" || (pass.length) < 6) {
+        document.getElementById("pass").style.borderColor = "red";
+        document.getElementById("error_message7").innerHTML = ("Password must be less than 6");
+        count=true;
+        return;
     } else {
-        var y = document.getElementById("pin").value;
-        if (y.length != 6) {
-            window.alert("Length of Pin must be eqaul to 6");
+        var b = document.getElementById("pass").value;
+        if (!(b.includes("@") || b.includes("#") || b.includes("&") || b.includes("&"))) {
+            document.getElementById("pass").style.borderColor = "red";
+            document.getElementById("error_message7").innerHTML = ("Password must contain a special character");
+            return;
         } else {
-            var z = document.getElementById("adre").value;
-            if (z == "") {
-                window.alert("Address must not be empty");
-
-            } else {
-
-                var a = document.getElementById("month").value;
-                if (a == "") {
-                    window.alert("Month must not be empty");
-
-                } else {
-                    var k = document.getElementById("date").value;
-                    if (k == "") {
-                        window.alert("Date must not be empty");
-
-                    } else {
-
-                        var b = document.getElementById("pass").value;
-                        if (!(b.includes("@") || b.includes("#") || b.includes("&") || b.includes("&"))) {
-
-                            window.alert("Password must contain a special character");
-
-                        } else {
-                            if (b.length < 8) {
-                                window.alert("Password length must be more than 8");
-
-                            } else {
-                                if (!(document.getElementById("rad1").checked || document.getElementById("rad1").checked)) {
-                                    window.alert("One Radio Button must be selected");
-                                } else {
-
-                                    var img = document.getElementById('file').value;
-                                    if (img == "") {
-                                        window.alert("Image File must be selected");
-
-                                    } else {
-                                        window.alert("All Values are Validated");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            document.getElementById("pass").style.borderColor = "black";
+            document.getElementById("error_message7").innerHTML = ("");
+            return;
         }
     }
+}
+
+function checkEmail() {
+    var email = document.getElementById("email").value;
+    var mail = /[^@]+@[a-zA-Z]+\.[a-zA-Z]{2,6}/
+    if (mail.test(email)) {
+        {
+            document.getElementById("email").style.borderColor = "black";
+            document.getElementById("error_message4").innerHTML = ("");
+        }
+        return;
+
+    } else {
+        document.getElementById("email").style.borderColor = "red";
+        count=true;
+        document.getElementById("error_message4").innerHTML = ("not a mail id");
+        return;
+
+    }
+}
+
+function checkName() {
+    var name = document.getElementById("value").value;
+    if (name.length < 6) {
+        document.getElementById("value").style.borderColor = "red";
+        document.getElementById("error_message2").innerHTML = ("Name  Must contains atleast 6 Chracters");
+        count=true;
+        return;
+    } else if (/^[a-zA-Z]+$/.test(name)) {
+        document.getElementById("value").style.borderColor = "black";
+        document.getElementById("error_message2").innerHTML = ("");
+        return;
+    } else {
+        document.getElementById("value").style.borderColor = "red";
+        document.getElementById("error_message2").innerHTML = ("Name  Must contains No Numbers");
+        return;
+    }
+
+
+}
+
+function checkNumber() {
+    var mobile = document.getElementById("mobile").value;
+    if (mobile.length != 10) {
+        document.getElementById("mobile").style.borderColor = "red";
+        document.getElementById("error_message11").innerHTML = ("Mobile Number Must contains 10 Numbers");
+        count=true;
+        return;
+    } else {
+        document.getElementById("error_message11").innerHTML = ("");
+        document.getElementById("mobile").style.borderColor = "black";
+
+        return;
+    }
+
+}
+
+
+function checkPin() {
+    var pin = document.getElementById("pin").value;
+    if (pin.length != 6) {
+        document.getElementById("pin").style.borderColor = "red";
+        document.getElementById("error_message1").innerHTML = ("Pin Code Must contains 6 Numbers");
+        count = true;
+        return;
+
+
+    } else {
+        document.getElementById("pin").style.borderColor = "black";
+        document.getElementById("error_message1").innerHTML = ("");
+        return;
+    }
+
+}
+
+/* 
+function checkDate() {
+    var date = document.getElementById("date").value;
+    if (date == "") {
+        document.getElementById("date").style.borderColor = "red";
+        document.getElementById("error_message6").innerHTML = ("Date must not be empty");
+    	count=true;
+        return;
+    } else {
+        {
+            document.getElementById("date").style.borderColor = "black";
+            document.getElementById("error_message6").innerHTML = ("");
+            return;
+        }
+    }
+
+}
+
+function checkMonth() {
+    var month = document.getElementById("month").value;
+    if (month == "") {
+        document.getElementById("month").style.borderColor = "red";
+        document.getElementById("error_message5").innerHTML = ("Month must not be empty");
+        return;
+    } else {
+        {
+            document.getElementById("month").style.borderColor = "black";
+            document.getElementById("error_message5").innerHTML = ("");
+        }
+        return;
+    }
+
+
+}
+ */
+
+function checkAddress() {
+    var address = document.getElementById("adress").value;
+    if (address == "") {
+        document.getElementById("adress").style.borderColor = "red";
+        document.getElementById("error_message3").innerHTML = ("Address must not be empty");
+        return;
+    } else {
+        {
+            document.getElementById("adress").style.borderColor = "black";
+            document.getElementById("error_message3").innerHTML = ("");
+        }
+        return;
+
+    }
+
+}
+
+function checkFile() {
+    var img = document.getElementById("file").value;
+
+    if (img == "") {
+        document.getElementById("file").style.borderColor = "red";
+        document.getElementById("error_message8").innerHTML = ("Image File must be selected");
+        return;
+
+    } else {
+        {
+            document.getElementById("error_message8").innerHTML = ("");
+            document.getElementById("file").style.borderColor = "black";
+
+            return;
+        }
+
+
+    }
+
+
+}
+
+function checkRadio() {
+    if (!(document.getElementById("rad1").checked || document.getElementById("rad2").checked)) {
+        document.getElementById("error_message9").innerHTML = ("One Radio Button must be selected");
+        return;
+    } else {
+        document.getElementById("error_message9").innerHTML = (""); 
+        return;
+    }
+
+}
+
+function checkCheck() {
+    if (!(document.getElementById("vehicle1").checked || document.getElementById("vehicle2").checked)) {
+        document.getElementById("error_message10").innerHTML = ("One Check Box must be selected");
+        return;
+    } else {
+        {
+            document.getElementById("error_message10").innerHTML = ("");
+            if(count)
+            {
+            	alert("Not Validated")
+            }
+            else
+            {
+            	alert("All Values are Validated")
+            }
+        }
+        return;
+    }
+
 }
--- form/jq/valid1.js
+++ form/jq/valid1.js
@@ -1,268 +1,253 @@
 $(document).ready(function () {
 	var value = false;
 	var submit = false;
-	var count = 0;
-	$("#SubjectsList").change(function () {
-		var selectedSubject = $("#SubjectsList option:selected").val();
-		alert("You have selected the country - " + selectedSubject);
-	});
+	var count = false;
+	var count1=false;
 	$("#hiding").click(function () {
 		$("form").toggle();
-
 	});
 	$("#submit").click(function () {
-		var b = $("#pass").val();
-		submit = true; {
-			count=0;
+			submit = true; {
+			count=false;
+			var count1=false;
 			checkPin();
 			checkName();
 			checkAddress();
 			checkEmail();
-			checkMonth();
-			checkDate();
+			//checkMonth();
+			//checkDate();
 			checkPassword();
 			checkNumber();
 			checkFile();
 			checkCheck();
 			checkRadio();
-			if(count==0){
-				window.alert("All Values are Validated");
-				$("#error_message").text("");
-			}
 			submit = false;
+			count=false;
 		}
-
-
 	});
-	var myModal = document.getElementById("modalDemo");
-	var myButton = document.getElementById("modalBtn");
-	var exitBtn = document.getElementsByClassName("exit")[0];
-	myButton.onclick = function () {
-		myModal.style.display = "block";
-	}
-	exitBtn.onclick = function () {
-		myModal.style.display = "none";
-	}
-	window.onclick = function (event) {
-		if (event.target == myModal) {
-			myModal.style.display = "none";
-		}
-	}
 });
 
 function checkPassword() {
-	var pass = $("#pass").val();
-	if (pass == "" || (pass.length) < 6) {
-		$("#error_message7").text("Password must be less than 6");
-		return;
-	} else {
-		var b = $("#pass").val();
-		if (!(b.includes("@") || b.includes("#") || b.includes("&") || b.includes("&"))) {
-			$("#error_message7").text("Password must contain a special character");
-			$("#pass").css({
-				"border": "1px solid red"
-			});
-
-			count++;
-			return;
-		} else {
-			 {
-				$("#error_message7").text("");
-			}
-			$("#pass").css({
-				"border": "1px solid black"
-			});
-			return;
-		}
-	}
+    var pass = $("#pass").val();
+    if (pass == "" || (pass.length) < 6) {
+        $("#error_message7").text("Password must be less than 6");
+        return;
+    } else {
+        var b = $("#pass").val();
+        if (!(b.includes("@") || b.includes("#") || b.includes("&") || b.includes("&"))) {
+            $("#error_message7").text("Password must contain a special character");
+            $("#pass").css({
+                "border": "1px solid red"
+            });
+            count1 = true;
+            return;
+        } else {
+            {
+                $("#error_message7").text("");
+            }
+            $("#pass").css({
+                "border": "1px solid black"
+            });
+            return;
+        }
+    }
 }
 
 function checkEmail() {
-	var email = $("#email").val();
-	var mail = /[^@]+@[a-zA-Z]+\.[a-zA-Z]{2,6}/
-	if (mail.test(email)) {
-		 {
-			$("#error_message4").text("");
-		}
-		$("#email").css({
-			"border": "1px solid black"
-		});
-		return;
-	
-	} else {
-		$("#email").css({
-			"border": "1px solid red"
-		});
-		$("#error_message4").text("not a mail id");
-		return;
-		count++;
-		
-	}
+    var email = $("#email").val();
+    var mail = /[^@]+@[a-zA-Z]+\.[a-zA-Z]{2,6}/
+    if (mail.test(email)) {
+        {
+            $("#error_message4").text("");
+        }
+        $("#email").css({
+            "border": "1px solid black"
+        });
+        return;
+
+    } else {
+        $("#email").css({
+            "border": "1px solid red"
+        });
+        count1 = true;
+        $("#error_message4").text("not a mail id");
+        return;
+
+    }
 }
 
 function checkName() {
-	var name = $("#value").val();
-	if (name.length < 6) {
-		$("#error_message2").text("Name  Must contains atleast 6 Chracters");
-		return;
-		$("#value").css({
-			"border": "1px solid red"
-		});
-		count++;
-	} else if (/^[a-zA-Z]+$/.test(name)) {
-			$("#error_message2").text("");
-			$("#value").css({
-			"border": "1px solid black"
-		});
-		return;
-	} else {
-		$("#value").css({
-			"border": "1px solid red"
-		});
-		$("#error_message2").text("Name  Must contains No Numbers");
-		count++;
-		return;
-	}
+    var name = $("#value").val();
+    if (name.length < 6) {
+        $("#error_message2").text("Name  Must contains atleast 6 Chracters");
+        return;
+        $("#value").css({
+            "border": "1px solid red"
+        });
+        count1 = true;
+    } else if (/^[a-zA-Z]+$/.test(name)) {
+        $("#error_message2").text("");
+        $("#value").css({
+            "border": "1px solid black"
+        });
+        return;
+    } else {
+        $("#value").css({
+            "border": "1px solid red"
+        });
+        $("#error_message2").text("Name  Must contains No Numbers");
+        return;
+    }
 
 
 }
 
 function checkNumber() {
-	var y = $("#mobile").val();
-	if (y.length != 10) {
-		$("#error_message11").text("Mobile Number Must contains 10 Numbers");
-		$("#mobile").css({
-			"border": "1px solid red"
-		});
-		return;
-	} else {
-		 {
-			$("#error_message11").text("");
-		}
-			$("#mobile").css({
-			"border": "1px solid black"
-		});
-		return;
-	}
+    var mobile = $("#mobile").val();
+    if (mobile.length != 10) {
+        $("#error_message11").text("Mobile Number Must contains 10 Numbers");
+        $("#mobile").css({
+            "border": "1px solid red"
+        });
+        count1 = true;
+        return;
+    } else {
+        {
+            $("#error_message11").text("");
+        }
+        $("#mobile").css({
+            "border": "1px solid black"
+        });
+        return;
+    }
 
 }
 
 
 function checkPin() {
-	var y = $("#pin").val();
-	if (y.length != 6) {
-		$("#error_message1").text("Pin Code Must contains 6 Numbers");
-		$("#pin").css({
-			"border": "1px solid red"
-		});
-		return;
+    var pin = $("#pin").val();
+    if (pin.length != 6) {
+        $("#error_message1").text("Pin Code Must contains 6 Numbers");
+        $("#pin").css({
+            "border": "1px solid red"
+        });
+        count1 = true;
+        return;
 
 
-		count++;
-	} else {
-		 {
-			$("#error_message1").text("");
-		}
-			$("#pin").css({
-			"border": "1px solid black"
-		});
-		return;
-	}
+    } else {
+        {
+            $("#error_message1").text("");
+        }
+        $("#pin").css({
+            "border": "1px solid black"
+        });
+        count1 = false;
+        return;
+    }
 
 }
 
+/* 
 function checkDate() {
-	var k = $("#date").val();
-	if (k == "") {
-		$("#error_message6").text("Date must not be empty");
-		return;
-	} else {
-		 {
-			$("#error_message6").text("");
-			return;
-		}
-	}
+    var date = $("#date").val();
+    if (date == "") {
+        $("#error_message6").text("Date must not be empty");
+        return;
+    } else {
+        {
+            $("#error_message6").text("");
+            return;
+        }
+    }
 
 }
 
 function checkMonth() {
-	var a = $("#month").val();
-	if (a == "") {
-		$("#error_message5").text("Month must not be empty");
-		return;
-	} else {
-		 {
-			$("#error_message5").text("");
-		}
-		return;
-	}
+    var month = $("#month").val();
+    if (month == "") {
+        $("#error_message5").text("Month must not be empty");
+        return;
+    } else {
+        {
+            $("#error_message5").text("");
+        }
+        return;
+    }
 
 
 }
+ */
 
 function checkAddress() {
-	var z = $("#adress").val();
-	if (z == "") {
-		$("#error_message3").text("Address must not be empty");
-		return;
-	} else {
-		 {
-			$("#error_message3").text("");
-		}
-		return;
+    var address = $("#adress").val();
+    if (address == "") {
+        $("#error_message3").text("Address must not be empty");
+        count1 = true;
+        return;
+    } else {
+        {
+            $("#error_message3").text("");
+        }
+        return;
 
-	}
+    }
 
 }
 
 function checkFile() {
-	var img = $("#file").val();
+    var img = $("#file").val();
 
-	if (img == "") {
-		$("#error_message8").text("Image File must be selected");
-		$("#file").css({
-			"border": "1px solid red"
-		});
-		return;
+    if (img == "") {
+        $("#error_message8").text("Image File must be selected");
+        $("#file").css({
+            "border": "1px solid red"
+        });
+        count1 = true;
+        return;
 
-	} else {
-		 {
-			$("#error_message8").text("");
-		$("#file").css({
-			"border": "1px solid black"
-		});	
-		return;	
-		}
-		
+    } else {
+        {
+            $("#error_message8").text("");
+            $("#file").css({
+                "border": "1px solid black"
+            });
+            return;
+        }
 
-	}
+
+    }
 
 
 }
 
 function checkRadio() {
-	if (!(document.getElementById("rad1").checked || document.getElementById("rad1").checked)) {
-		$("#error_message9").text("One Radio Button must be selected");
-		count++;
-		return;
-	} else {
-	 {
-			$("#error_message9").text("");
-		}
-		return;
-	}
+    if (!(document.getElementById("rad1").checked || document.getElementById("rad2").checked)) {
+        $("#error_message9").text("One Radio Button must be selected");
+        return;
+    } else {
+            $("#error_message9").text("");
+            if(count1)
+            {
+            }
+            else
+            {
+                alert("All Values are Validated")
+            }
+        return;
+    }
 
 }
+
 function checkCheck() {
-	if (!(document.getElementById("vehicle1").checked || document.getElementById("vehicle2").checked)) {
-		$("#error_message10").text("One Check Box must be selected");
-		count++;
-		return;
-	} else {
-	 {
-			$("#error_message10").text("");
-		}
-		return;
-	}
+    if (!(document.getElementById("vehicle1").checked || document.getElementById("vehicle2").checked)) {
+        $("#error_message10").text("One Check Box must be selected");
+        return;
+    } else {
+        {
+            $("#error_message10").text("");
+        }
+        return;
+    }
 
 }
